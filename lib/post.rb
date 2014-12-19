f2aba2d31be04feb0d94d8496fde83eaeb2696dd
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2014-12-19 22:22:06 +0000

    changed from maruku to kramdown. Simplified syntax highlighting

--- lib/post.rb
+++ lib/post.rb
@@ -4,76 +4,76 @@ require 'pry'
 
 class Post < Sequel::Model
   plugin :schema
-	unless table_exists?
-		set_schema do
-			primary_key :id
-			text :title
-			text :body
-			text :slug
-			text :tags
-			timestamp :created_at
-		end
-		create_table
-	end
+  unless table_exists?
+    set_schema do
+      primary_key :id
+      text :title
+      text :body
+      text :slug
+      text :tags
+      timestamp :created_at
+    end
+    create_table
+  end
 
-	def url
-		d = created_at
-		"/past/#{d.year}/#{d.month}/#{d.day}/#{slug}/"
-	end
+  def url
+    d = created_at
+    "/past/#{d.year}/#{d.month}/#{d.day}/#{slug}/"
+  end
 
-	def full_url
-		Blog.url_base.gsub(/\/$/, '') + url
-	end
+  def full_url
+    Blog.url_base.gsub(/\/$/, '') + url
+  end
 
-	def body_html
-		to_html(body)
-	end
+  def body_html
+    to_html(body)
+  end
 
-	def summary
-		@summary ||= body.match(/(.{200}.*?\n)/m)
-		@summary || body
-	end
+  def summary
+    @summary ||= body.match(/(.{200}.*?\n)/m)
+    @summary || body
+  end
 
-	def summary_html
-		to_html(summary.to_s)
-	end
+  def summary_html
+    to_html(summary.to_s)
+  end
 
-	def more?
-		@more ||= body.match(/.{200}.*?\n(.*)/m)
-		@more
-	end
+  def more?
+    @more ||= body.match(/.{200}.*?\n(.*)/m)
+    @more
+  end
 
-	def linked_tags
-		tags.split.inject([]) do |accum, tag|
-			accum << "<a href=\"/past/tags/#{tag}\">#{tag}</a>"
-		end.join(" ")
-	end
+  def linked_tags
+    tags.split.inject([]) do |accum, tag|
+      accum << "<a href=\"/past/tags/#{tag}\">#{tag}</a>"
+    end.join(" ")
+  end
 
-	def self.make_slug(title)
-		title.downcase.gsub(/ /, '_').gsub(/[^a-z0-9_]/, '').squeeze('_')
-	end
+  def self.make_slug(title)
+    title.downcase.gsub(/ /, '_').gsub(/[^a-z0-9_]/, '').squeeze('_')
+  end
 
-	########
+  ########
 
-	def to_html(markdown)
+  def to_html(markdown)
     #binding.pry
     Kramdown::Document.new(markdown, {
       :syntax_highlighter => 'coderay',
       :syntax_highlighter_opts => { :default_lang => 'ruby'}
     }).to_html
-	end
+  end
 
-	def split_content(string)
-		parts = string.gsub(/\r/, '').split("\n\n")
-		show = []
-		hide = []
-		parts.each do |part|
-			if show.join.length < 100
-				show << part
-			else
-				hide << part
-			end
-		end
-		[ to_html(show.join("\n\n")), hide.size > 0 ]
-	end
+  def split_content(string)
+    parts = string.gsub(/\r/, '').split("\n\n")
+    show = []
+    hide = []
+    parts.each do |part|
+      if show.join.length < 100
+        show << part
+      else
+        hide << part
+      end
+    end
+    [ to_html(show.join("\n\n")), hide.size > 0 ]
+  end
 end
